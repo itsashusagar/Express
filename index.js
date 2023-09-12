88e85f19d15a07a187b04bd8f59af38d454701f0
ashu sagar <itsashusagar@gmail.com>
2023-09-12 11:20:46 +0530

    commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+const PORT = 3000;
+
+app.set('view engine', 'ejs')
+app.use(express.static('views'))
+
+app.get('/' , (req, res) => {
+    res.render('index', {
+        pageTitle: "Ashu Portfolio",
+        name: "Ashu",
+        currentDate: new Date().toDateString()
+    })
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is Started on Port ${PORT}`)
+})
